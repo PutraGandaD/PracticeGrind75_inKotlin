93b12370918165a06573ce44d20827b8173d8e77
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2024-05-14 08:29:59 +0700

    Finished FizzBuzz

--- LeetCode/src/main/kotlin/FizzBuzz.kt
+++ LeetCode/src/main/kotlin/FizzBuzz.kt
@@ -1,8 +1,19 @@
-fun fizzBuzz(n: Int) {
+fun fizzBuzz(n: Int): List<String> {
     var result: List<String> = listOf()
 
     for(i in 1..n) {
+        if(i % 3 == 0 && i % 5 == 0) {
+            result += "FizzBuzz"
+        } else if(i % 3 == 0) {
+            result += "Fizz"
+        } else if(i % 5 == 0) {
+            result += "Buzz"
+        } else {
+            result += "$i"
+        }
     }
+
+    return result
 }
 
 fun main() {
