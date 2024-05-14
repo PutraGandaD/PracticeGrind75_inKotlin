cb327da328f614efa84d4c19447cd9d544a2eb0d
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2024-05-14 10:23:09 +0700

    Finished Invert Binary Tree

--- LeetCode/src/main/kotlin/InvertBinaryTree.kt
+++ LeetCode/src/main/kotlin/InvertBinaryTree.kt
@@ -1,2 +1,13 @@
-class InvertBinaryTree {
+fun invertTree(root: TreeNode?): TreeNode? {
+    root ?: return null
+
+    val temp = root.left // save root left to temp
+    root.left = invertTree(root.right) // recursive, invert right root and place it to root left
+    root.right = invertTree(temp) // recursive, invert left root (we prev save on temp), and place it to root right
+
+    return root // return root of the tree
+}
+
+fun main() {
+
 }
